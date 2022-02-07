3f3626694857d0f6cea16f2b02196cba094cdb66
inafontan@gmail.com <you@example.com>
2022-02-07 18:11:18 -0300

    Desafío 1 - Fontán Roxana

---
+++ script.js
@@ -0,0 +1,35 @@
+let texto = prompt ("Ingrese su carrera para continuar! (Profesorado / Ciclo)")
+
+if(texto === "Profesorado"){
+    prompt("Ingrese su DNI")
+    alert("Bienvenido")
+    
+    let totalCuotas = 31
+    let numero1 = prompt("Para consultar la cantidad de cuotas que resta abonar, ingrese cantidad de cuotas pagas");
+ 
+    const cuotasPagas = (totalCuotas,numero1) =>  totalCuotas - numero1;
+
+    alert(`Resta abonar cuotas ${cuotasPagas(totalCuotas,numero1)}`)
+
+    let acumulador = 0;
+    let cantidadNotas = 5;
+
+    do { 
+    let nota = parseInt(prompt("Para conocer su promedio, ingrese las notas"))
+    acumulador += nota
+    var confirmacion = prompt("¿Desea ingresar otra nota, enter, de lo contrario no?")   
+    cantidadNotas++
+    } while(confirmacion != "no")
+
+    console.log(`El promedio de las notas es ${acumulador / cantidadNotas}`)
+
+} else {
+    alert ("Complete el formulario para registrarse")
+    prompt ("Ingrese su nombre")
+    alert ("Va a  inscribirse al Profesorado")
+
+    console.log ("Usted se Inscribió correctamente")
+}
+     
+
+                                
